53897606eab7c8425470c677b98b40cf0cdc3647
Martin Fernandez <Electronicsmf@aol.com>
2022-10-18 21:21:20 -0400

    Adding Get Doctors Part 1

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,36 @@
+﻿using MyAppointment.Data;
+using MyAppointment.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAppointment.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<DoctorVM> GetDoctorList()
+        {
+            var doctors = (from user in _context.Users
+                           select new DoctorVM
+                           {
+                               Id = user.Id,
+                               Name = user.Name
+                           }
+                        ).ToList();
+
+            return doctors;
+        }
+
+        public List<PatientVM> GetPatientList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IAppointmentService.cs
@@ -0,0 +1,14 @@
+﻿using MyAppointment.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAppointment.Services
+{
+    public interface IAppointmentService
+    {
+        public List<DoctorVM> GetDoctorList();
+        public List<PatientVM> GetPatientList();
+    }
+}
